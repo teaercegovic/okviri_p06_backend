78e1159280e2a7874517d836b7973571145ea3f6
gzaharija <gzaharija@pmfst.hr>
2021-11-10 11:54:11 +0100

    Uklonjen build iz gitignore

---
+++ build/precache-manifest.3158c6363a2b9ca87891d851bfcbfb40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b3fc1db353d25c5bc8cb4ea2f11744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22ae4cfb4d25e14cb5f7",
+    "url": "/static/css/main.a5670662.chunk.css"
+  },
+  {
+    "revision": "43b6cea0238e39ea0840",
+    "url": "/static/js/2.cebc3bdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cebc3bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ae4cfb4d25e14cb5f7",
+    "url": "/static/js/main.7b106868.chunk.js"
+  },
+  {
+    "revision": "b5942b035685b24b37e7",
+    "url": "/static/js/runtime-main.0a493ba8.js"
+  }
+]);
